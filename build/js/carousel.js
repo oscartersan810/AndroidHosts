fac5b242ca7f5ccbc64d0edc3ec0d04db56b4e1a
oscartersan810 <162900750+oscartersan810@users.noreply.github.com>
2024-03-10 19:09:50 +0100

    Add files via upload

---
+++ build/js/carousel.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    // Inicia el carousel y configura la transición automática cada 5 segundos
+    $("#my-carousel").carousel({
+      interval: 5000 // 5000 milisegundos = 5 segundos
+    });
+  });
+  
